3074852601d9fded5f8b596dfce8a0bf209702e5
criscoelhor <coelhocr@gmail.com>
2023-02-06 15:14:58 -0300

    first commit

---
+++ src/FC.Codeflix.Catalog.Application/Interfaces/IStorageService.cs
@@ -0,0 +1,11 @@
+﻿namespace FC.Codeflix.Catalog.Application.Interfaces;
+
+public interface IStorageService
+{
+    Task Delete(string filePath, CancellationToken cancellationToken);
+
+    Task<string> Upload(
+        string fileName, 
+        Stream fileStream, 
+        CancellationToken cancellationToken);
+}
